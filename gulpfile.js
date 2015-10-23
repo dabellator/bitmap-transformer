e1f33f80474a152ab1682a6e4e9216900a3eb468
Craig Campbell <craig.campbell8@gmail.com>
2015-10-23 10:30:52 -0700

    changed a few comments

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp = require('gulp')
+var jshint = require('gulp-jshint');
+var jscs = require('jscs');
+var gulpMocha = require('gulp-mocha');
+
+gulp.task('jshint', function(){
+  return gulp.src(['.gulpfile.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function(){
+  return gulp.src('test/**/*Test.js')
+    .pipe(gulpMocha({reporter: 'nyan'}));
+    // .pipe(gulpMocha({reporter: 'mocha-lcov-reporter'}));
+    // .pipe(gulpMocha({reporter: 'mocha-notifier-reporter'}));
+});
+
+
+gulp.task('default', ['jshint', 'test'])
