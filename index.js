e6dd10789800d80eea71d9e62b61b32de0fc565a
Jordan Bundy <jordanbundy@MacBook-Air-5.local>
2015-10-31 15:55:37 -0700

    added event emitters to index, index runs a static version, no .argv usage yet

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-var Reader = require('./read');
+var EE = require('events').EventEmitter;
+var process = new EE();
+var reader = require('./read');
 var modifier = require('./transform');
+var bitmap = {};
 
-var test = new Reader('palette-bitmap.bmp');
-console.log('original: ' + test.colorPalette[0], test.colorPalette[1], test.colorPalette[2], test.colorPalette[3]);
-test.applyFilter(modifier.transform, 'green');
-console.log(test.colorPalette);
-// console.log('changed: ' + changed[0], changed[1], changed[2], changed[3]);
-// test.colorPalette = changed;
-test.write('another2.bmp');
+process.on('open', function(file) {
+
+  bitmap = reader(file);
+  process.emit('modify', 'transform', 'red');
+});
+
+process.on('modify', function(filter, type) {
+
+  bitmap.applyFilter(modifier[filter], type);
+  process.emit('write', 'event.bmp');
+});
+
+process.on('write', function(name) {
+
+  bitmap.write(name);
+});
+
+process.emit('open', 'palette-bitmap.bmp');
 
